99cce2c5ad90210067e5775000afb71e52e42f16
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-13 20:47:00 +1300

    Add IssueModel

---
+++ RepositoryPractice2/Models/tblIssue.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RepositoryPractice2.Models
+{
+    public class tblIssue
+    {
+        [Key]
+        public int IssueId { get; set; }
+        [Required]
+        public string Problem { get; set; }
+        [Required]
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        [Required]
+        public string CarRego { get; set; }
+        public string Description { get; set; }
+        public string PaymentStatus { get; set; }
+        public string WorkStatus { get; set; }
+
+        public int CustomerId { get; set; }
+    }
+}
