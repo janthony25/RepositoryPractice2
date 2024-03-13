85fcb6543fd8d7c3460c0cb5996c628609365326
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-13 21:05:51 +1300

    add dbcontext to Program.cs

---
+++ RepositoryPractice2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPractice2.Models;
+
+namespace RepositoryPractice2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<tblCustomer> tblCustomer { get; set; }
+        public DbSet<tblIssue> tblIssue { get; set; }
+    }
+}
