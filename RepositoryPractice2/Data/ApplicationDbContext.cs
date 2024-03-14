fbd9201adea91fed1574c29740d34e374f153995
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-14 22:20:37 +1300

    Add ViewTblCustomerWithIssues in DBContext

--- RepositoryPractice2/Data/ApplicationDbContext.cs
+++ RepositoryPractice2/Data/ApplicationDbContext.cs
@@ -13,5 +13,23 @@ namespace RepositoryPractice2.Data
         public DbSet<tblCustomer> tblCustomer { get; set; }
         public DbSet<tblIssue> tblIssue { get; set; }
 
+        public DbSet<ViewTblCustomerWithIssue> View_CustomerWithIssues { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ViewTblCustomerWithIssue>(Entity =>
+            {
+                Entity.HasNoKey().ToView("View_CustomerWithIssues");
+                Entity.Property(e => e.CustomerId).HasColumnName("CustomerId");
+                Entity.Property(e => e.CustomerName).HasMaxLength(50).HasColumnName("CusomerName");
+                Entity.Property(e => e.Problem).HasMaxLength(50).HasColumnName("Problem");
+                Entity.Property(e => e.PaymentStatus).HasMaxLength(10).HasColumnName("PaymentStatus");
+                Entity.Property(e => e.WorkStatus).HasMaxLength(10).HasColumnName("WorkStatus");
+
+                
+            }); 
+               
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
