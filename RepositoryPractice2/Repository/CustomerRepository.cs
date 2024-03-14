12b5b716f1438aacc72b7261b608e8cd0ea3f41f
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-14 22:26:55 +1300

    Add CustomerRepository

---
+++ RepositoryPractice2/Repository/CustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPractice2.Data;
+using RepositoryPractice2.Models;
+
+namespace RepositoryPractice2.Repository
+{
+    public class CustomerRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private DbSet<tblCustomer> CustomerEntity;
+        private DbSet<tblIssue> IssueEntity;
+        public CustomerRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<ViewTblCustomerWithIssue> GetCustomer()
+        {
+            var customerWithIssue = _db.Set<ViewTblCustomerWithIssue>();
+            return customerWithIssue.AsEnumerable();
+        }
+    }
+}
